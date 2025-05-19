2b503c67673095bdb24d33dd5f1ef0a162784c15
anna-glass <anna@getslate.co>
2025-05-19 03:48:52 +0100

    fixes id problem

--- constants/org-chart-cypher.ts
+++ constants/org-chart-cypher.ts
@@ -2,8 +2,8 @@ export const CYPHER = `
 MATCH (f:Founder)-[:EMPLOYEES_AT]->(c:Company)-[:HAS_PARTNER]->(p:Partner)
 WITH f, p, collect(c.name) AS shared_companies
 RETURN 
-  apoc.map.merge(properties(f), {labels: labels(f)}) AS person1, 
-  apoc.map.merge(properties(p), {labels: labels(p)}) AS person2, 
+  apoc.map.merge(properties(f), {labels: labels(f), elementId: elementId(f)}) AS person1, 
+  apoc.map.merge(properties(p), {labels: labels(p), elementId: elementId(p)}) AS person2, 
   apoc.create.vRelationship(f, 'SHARES_COMPANY_WITH', {companies: shared_companies}, p) AS rel
 
 UNION ALL
@@ -12,7 +12,7 @@ MATCH (f1:Founder)-[:EMPLOYEES_AT]->(c:Company)<-[:EMPLOYEES_AT]-(f2:Founder)
 WHERE elementId(f1) < elementId(f2)
 WITH f1, f2, collect(c.name) AS shared_companies
 RETURN 
-  apoc.map.merge(properties(f1), {labels: labels(f1)}) AS person1, 
-  apoc.map.merge(properties(f2), {labels: labels(f2)}) AS person2, 
+  apoc.map.merge(properties(f1), {labels: labels(f1), elementId: elementId(f1)}) AS person1, 
+  apoc.map.merge(properties(f2), {labels: labels(f2), elementId: elementId(f2)}) AS person2, 
   apoc.create.vRelationship(f1, 'COFOUNDER_AT', {companies: shared_companies}, f2) AS rel
 `;
